8366f1e85ed1b6f1ec6bf81d7a2e67b8cca1e714
Stéphane Clairbaux <stephane@clairbaux.com>
2020-12-13 13:31:34 +0100

    perf: draw updated cells

--- index.js
+++ index.js
@@ -1,51 +1,42 @@
 import Stats from './stats.js'
-const stats = new Stats()
+import { init, step } from './life.js'
 
-const SIZE = 4
-const COLS = 120
-const ROWS = 120
-const ALIVE = 'hsl(0, 0%, 90%)'
-const DEAD = 'hsl(0, 0%, 10%)'
-
-const initialState = [...Array(COLS * ROWS)].map(() => Math.random() > 0.8 ? 1 : 0)
-
-const neighbours = []
-for (let i = 0; i < ROWS; i++) {
-  for (let j = 0; j < COLS; j++) {
-    const [n, s, w, e] = [i - 1, i + 1, j - 1, j + 1]
-    neighbours[i * COLS + j] =
-      [[n, w], [n, j], [n, e], [i, w], [i, e], [s, w], [s, j], [s, e]]
-        .map(([i, j]) => (i % ROWS) * COLS + (j % COLS))
-  }
-}
-
-const step = (state) => state.map((cell, k) => {
-  const count = neighbours[k].map((i) => state[i]).reduce((a, b) => a + b, 0)
-  return count === 3 ? 1 : count !== 2 ? 0 : cell
-})
-
-const canvas = document.createElement('canvas')
-canvas.width = COLS * SIZE
-canvas.height = ROWS * SIZE
+const size = 2
+const cols = 240
+const rows = 240
+const aliveColor = 'hsl(0, 0%, 90%)'
+const deadColor = 'hsl(0, 0%, 10%)'
 
 const root = document.querySelector('#root')
-root.style.background = DEAD
+root.style.background = deadColor
+
+const canvas = document.createElement('canvas')
+canvas.width = cols * size
+canvas.height = rows * size
 root.appendChild(canvas)
+
+const stats = new Stats()
 root.appendChild(stats.dom)
 
 const ctx = canvas.getContext('2d')
-ctx.scale(SIZE, SIZE)
+ctx.scale(size, size)
 
-const render = (state) => state.forEach((cell, i) => {
-  ctx.fillStyle = cell === 1 ? ALIVE : DEAD
-  ctx.fillRect(Math.floor(i / ROWS), i % ROWS, 1, 1)
-})
+const renderCells = (cells, color) => {
+  cells.forEach((i) => {
+    ctx.fillStyle = color
+    ctx.fillRect(Math.floor(i / rows), i % rows, 1, 1)
+  })
+}
 
-let state = initialState
+const render = ({ alive, dead }) => {
+  renderCells(alive, aliveColor)
+  renderCells(dead, deadColor)
+}
+
+render(init(cols, rows))
 ;(function loop() {
   stats.begin()
-  render(state)
-  state = step(state)
+  render(step())
   stats.end()
   requestAnimationFrame(loop)
 })()
