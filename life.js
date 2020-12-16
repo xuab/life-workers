eaa10bcfbae1fa3219cc543b4748f43e25f77a73
Stéphane Clairbaux <stephane@clairbaux.com>
2020-12-16 21:01:52 +0100

    refac: life

--- life.js
+++ life.js
@@ -1,13 +1,4 @@
-let options = null
-let neighbours = null
-let world1 = null
-let world2 = null
-let diff1 = null
-let diff2 = null
-let locks = null
-
-const initNeighbours = () => {
-  const { cols, rows } = options
+const createNeighbours = (cols, rows) => {
   neighbours = [...Array(cols * rows)]
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
@@ -17,13 +8,10 @@ const initNeighbours = () => {
           .map(([i, j]) => (i % rows) * cols + (j % cols))
     }
   }
+  return neighbours
 }
 
-const initWorld = () => {
-  const { cols, rows } = options
-  world1 = [...Array(cols * rows)]
-  world2 = [...Array(cols * rows)]
-
+const init = (world1, diff1, locks) => {
   let index = 0
   for (let i = 0; i < diff1.length; i ++) {
     const cell = Math.random() > 0.8 ? 1 : 0
@@ -34,10 +22,9 @@ const initWorld = () => {
   Atomics.store(locks, 0, 0)
   Atomics.store(locks, 1, 1)
   Atomics.notify(locks, 1)
-  ;[diff1, diff2] = [diff2, diff1]
 }
 
-const step = () => {
+const step = (world1, world2, diff1, diff2, locks, neighbours) => {
   Atomics.wait(locks, 0, 0)
 
   let index = 0
@@ -58,23 +45,24 @@ const step = () => {
   Atomics.store(locks, 0, 0)
   Atomics.store(locks, 1, 1)
   Atomics.notify(locks, 1)
-  ;[diff1, diff2] = [diff2, diff1]
-  ;[world1, world2] = [world2, world1]
-  requestAnimationFrame(step)
+  requestAnimationFrame(() => step(world2, world1, diff2, diff1, locks, neighbours))
 }
 
 self.onmessage = (m) => {
   switch (m.data.type) {
     case 'init':
-      diff1 = new Int32Array(m.data.buffers.diff1)
-      diff2 = new Int32Array(m.data.buffers.diff2)
-      locks = new Int32Array(m.data.buffers.locks)
-      options = m.data.options
-      initNeighbours()
-      initWorld()
-      step()
+      const { buffers, options } = m.data
+      const { cols, rows } = options
+      const diff1 = new Int32Array(buffers.diff1)
+      const diff2 = new Int32Array(buffers.diff2)
+      const locks = new Int32Array(buffers.locks)
+      const world1 = [...Array(cols * rows)]
+      const world2 = [...Array(cols * rows)]
+      init(world1, diff1, locks)
+      const neighbours = createNeighbours(cols, rows)
+      step(world1, world2, diff2, diff1, locks, neighbours)
       return
     default:
-      return
+      throw new Error()
   }
 }
